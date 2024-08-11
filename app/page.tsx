afd361748f7a898d256c0b702f1e0a158214e580
HalilhanSAYIN <dev.halilhansayin@gmail.com>
2024-08-11 19:24:32 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import PixelArtMaker from "@/components/PixelArtMaker";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     
+     <PixelArtMaker/>
+    </main>
+  );
+}
